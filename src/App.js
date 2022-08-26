1f0fe3a806ef63a2439c792ab26549c343d819cd
JUNGJIN <ojj824@naver.com>
2022-08-26 21:27:32 +0900

    feat: tailwind css 설치

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
+import React from "react";
+import axios from "axios";
 
-import React from 'react';
-import axios from 'axios'
 function App() {
-	const loadbucket = async ()=>{
-		const res =	await axios.get("https://teamhomwork.herokuapp.com/comment")
-		return res.data
-	}
-	
-	React.useEffect(()=>{
-		loadbucket()
-	},[])
-	console.log(loadbucket())
-	return <div className="App">
-		asdasd
-	</div>;
+	const loadbucket = async () => {
+		const res = await axios.get("https://teamhomwork.herokuapp.com/comment");
+		return res.data;
+	};
+
+	React.useEffect(() => {
+		loadbucket();
+	}, []);
+	console.log(loadbucket());
+	return <div className="App">asdasd</div>;
 }
 
 export default App;
