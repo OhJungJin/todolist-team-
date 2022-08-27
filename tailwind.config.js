23abafe66f1389195c97fe77d701c8b3425ab74a
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-08-27 11:08:19 +0900

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
 };
