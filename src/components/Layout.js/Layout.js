cff2e4760c24fee11af0b6213574aa40b44c6f8b
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-08-27 18:01:10 +0900

    header component

--- src/components/Layout.js/Layout.js
+++ src/components/Layout.js/Layout.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React from 'react';
+import Header from 'components/Header/Header';
 
 const Layout = ({ children }) => {
-	return (
-		<>
-			<div className="w-screen h-screen max-h-[100%] min-w-[80px] max-w-[1200px] absolute left-1/2 -translate-x-1/2">
-				{children}
-			</div>
-		</>
-	);
+  return (
+    <div className="flex">
+      <Header />
+      <div className="w-screen h-screen max-h-[100%] min-w-[80px] max-w-[1200px]">
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
