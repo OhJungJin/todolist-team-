7c27cf4a391974e17012267346448776facaef3e
JUNGJIN <ojj824@naver.com>
2022-08-30 16:56:08 +0900

    feat: card id 삭제

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,38 +6,37 @@ import { useNavigate } from "react-router-dom";
 import { fetchTodo } from "redux/todoSlice";
 
 function Card() {
-  const dispatch = useDispatch();
-  const data = useSelector(state => state.todos);
-  const todos = data.data;
+	const dispatch = useDispatch();
+	const data = useSelector((state) => state.todos);
+	const todos = data.data;
 
-  useEffect(() => {
-    dispatch(fetchTodo(data));
-  }, []);
+	useEffect(() => {
+		dispatch(fetchTodo(data));
+	}, []);
 
-  let navigate = useNavigate();
+	let navigate = useNavigate();
 
-  // function moveDetail(id) {
-  // 	navigate(`/detail/${id}`);
-  // }
+	// function moveDetail(id) {
+	// 	navigate(`/detail/${id}`);
+	// }
 
-  return (
-    <>
-      {todos &&
-        todos.map(todo => (
-          <div
-            className="w-11/12 h-24 m-2 bg-gray-100"
-            onClick={() => {
-              navigate(`/detail/${todo.id}`);
-            }}
-            key={todo.id}
-          >
-            <div>{todo.id}</div>
-            <div>{todo.title}</div>
-            <div>{todo.content}</div>
-          </div>
-        ))}
-    </>
-  );
+	return (
+		<>
+			{todos &&
+				todos.map((todo) => (
+					<div
+						className="w-11/12 h-24 m-2 bg-gray-100"
+						onClick={() => {
+							navigate(`/detail/${todo.id}`);
+						}}
+						key={todo.id}
+					>
+						<div>{todo.title}</div>
+						<div>{todo.content}</div>
+					</div>
+				))}
+		</>
+	);
 }
 
 export default Card;
