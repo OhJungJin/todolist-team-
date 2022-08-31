a9de6a954757831c6039e2a88b4e7943f0b1748f
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-08-31 22:26:08 +0900

    comment 수정 기능 구현

---
+++ src/components/Comment/CommentItem.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { updateCommentThunk } from "redux/commentSlice";
+
+export default function CommentItem({
+  postCommentList,
+  postComment,
+  setPostCommentList,
+  deleteCommeent,
+}) {
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [comment, setComment] = useState(postComment);
+
+  const dispatch = useDispatch();
+
+  const handleToggleEditMode = () => {
+    if (isEditMode) setComment(postComment);
+    setIsEditMode((prev) => !prev);
+  };
+
+  const handleInputChange = ({ target: { name, value } }) => {
+    setComment({
+      ...comment,
+      [name]: value,
+    });
+  };
+
+  const handleEditSubmit = (e, id) => {
+    const payload = comment;
+    dispatch(updateCommentThunk({ id, payload }));
+    handleToggleEditMode();
+  };
+
+  useEffect(() => {
+    setComment(postComment);
+  }, [postComment]);
+
+  return (
+    <div className="flex flex-row items-center justify-between border-b-4 h20">
+      <div className="w-3/5">
+        {!isEditMode ? (
+          <>
+            <div>{postComment.userName}</div>
+            <div className="text-2xl">{postComment.desc}</div>
+          </>
+        ) : (
+          <>
+            <input
+              name="userName"
+              value={comment.userName}
+              onChange={handleInputChange}
+              className="block"
+            />
+            <input
+              type="text"
+              name="desc"
+              value={comment.desc}
+              className="text-2xl"
+              onChange={handleInputChange}
+              autoFocus
+            />
+          </>
+        )}
+        {isEditMode && (
+          <button onClick={(e) => handleEditSubmit(e, postComment.id)}>
+            수정완료
+          </button>
+        )}
+      </div>
+      <div className="flex items-center justify-center gap-4 mr-3">
+        <button onClick={handleToggleEditMode}>
+          {isEditMode ? "취소" : "수정"}
+        </button>
+        <button
+          onClick={() => {
+            deleteCommeent(postComment.id);
+          }}>
+          삭제
+        </button>
+      </div>
+    </div>
+  );
+}
