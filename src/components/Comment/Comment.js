8dbabdd1c2de2a778d76354d6f8043a981b6602a
JUNGJIN <ojj824@naver.com>
2022-08-31 19:17:10 +0900

    feat: Button 컴포넌트 추가

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -5,94 +5,93 @@ import { updateComment } from "redux/commentSlice";
 import EditComment from "./EditComment";
 
 function Comment({ userId, deleteCommeent }) {
-    const commentList = useSelector((state) => state.comments.list);
+	const commentList = useSelector((state) => state.comments.list);
 
-    const [isUpdate, setIsUpdate] = useState(true);
-    // const [updateComment, setUpdateComment] = useState("");
-    const [postcomment, setpostcomment] = useState(commentList);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [commentCount] = useState(4);
+	const [isUpdate, setIsUpdate] = useState(true);
+	// const [updateComment, setUpdateComment] = useState("");
+	const [postcomment, setpostcomment] = useState(commentList);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [commentCount] = useState(4);
 
-    const commentLength = commentList.length;
-    const maxPage =
-        Math.ceil(commentLength / commentCount) === 0
-            ? 1
-            : Math.ceil(commentLength / commentCount);
-    const lastIndex = currentPage * commentCount;
-    const firstIndex = lastIndex - commentCount;
-    console.log(commentList);
+	const commentLength = commentList.length;
+	const maxPage =
+		Math.ceil(commentLength / commentCount) === 0
+			? 1
+			: Math.ceil(commentLength / commentCount);
+	const lastIndex = currentPage * commentCount;
+	const firstIndex = lastIndex - commentCount;
 
-    React.useEffect(() => {
-        setpostcomment(commentList.slice(firstIndex, lastIndex));
-    }, [currentPage, commentCount]);
+	React.useEffect(() => {
+		setpostcomment(commentList.slice(firstIndex, lastIndex));
+	}, [currentPage, commentCount]);
 
-    return (
-        <div className="container h-full mx-auto">
-            <div className="border-2 border-emerald-400 h-3/5">
-                <EditComment></EditComment>
-                <div className="flex justify-between">
-                    <button
-                        disabled={currentPage <= 1}
-                        onClick={() => {
-                            setCurrentPage(currentPage - 1);
-                        }}
-                    >
-                        이전댓글
-                    </button>
-                    <div>{`${currentPage}/${maxPage} `}</div>
-                    <button
-                        disabled={currentPage >= maxPage}
-                        onClick={() => {
-                            setCurrentPage(currentPage + 1);
-                        }}
-                    >
-                        다음 댓글
-                    </button>
-                </div>
-                <div>
-                    {postcomment.map((x) => {
-                        return (
-                            <div
-                                key={x.id}
-                                className="flex flex-row items-center justify-between border-b-4 h20"
-                            >
-                                <div className="w-3/5">
-                                    <div>{x.userName}</div>
-                                    <div className="text-2xl ">{x.desc}</div>
-                                    {isUpdate === false ? (
-                                        <button
-                                            onClick={() => {
-                                                setIsUpdate(true);
-                                            }}
-                                        >
-                                            수정 완료
-                                        </button>
-                                    ) : null}
-                                </div>
-                                <div className="flex items-center justify-center gap-4 mr-3">
-                                    {/* 버튼기능 구현 */}
-                                    <button
-                                        onClick={() => {
-                                            setIsUpdate(!isUpdate);
-                                        }}
-                                    >
-                                        {isUpdate === true ? "수정" : "취소"}
-                                    </button>
-                                    <button
-                                        onClick={() => {
-                                            deleteCommeent(x.id);
-                                        }}
-                                    >
-                                        삭제
-                                    </button>
-                                </div>
-                            </div>
-                        );
-                    })}
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="container h-full mx-auto">
+			<div className="border-2 border-emerald-400 h-3/5">
+				<EditComment></EditComment>
+				<div className="flex justify-between">
+					<button
+						disabled={currentPage <= 1}
+						onClick={() => {
+							setCurrentPage(currentPage - 1);
+						}}
+					>
+						이전댓글
+					</button>
+					<div>{`${currentPage}/${maxPage} `}</div>
+					<button
+						disabled={currentPage >= maxPage}
+						onClick={() => {
+							setCurrentPage(currentPage + 1);
+						}}
+					>
+						다음 댓글
+					</button>
+				</div>
+				<div>
+					{postcomment.map((x) => {
+						return (
+							<div
+								key={x.id}
+								className="flex flex-row items-center justify-between border-b-4 h20"
+							>
+								<div className="w-3/5">
+									<div>{x.userName}</div>
+									<div className="text-2xl ">{x.desc}</div>
+									{isUpdate === false ? (
+										<button
+											onClick={() => {
+												setIsUpdate(true);
+											}}
+										>
+											수정 완료
+										</button>
+									) : null}
+								</div>
+								<div className="flex items-center justify-center gap-4 mr-3">
+									{/* 버튼기능 구현 */}
+									<button
+										onClick={() => {
+											setIsUpdate(!isUpdate);
+										}}
+									>
+										{isUpdate === true ? "수정" : "취소"}
+									</button>
+									<button
+										onClick={() => {
+											deleteCommeent(x.id);
+										}}
+									>
+										삭제
+									</button>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Comment;
