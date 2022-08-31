b2843447f43e2ebce77e5b8203c68f9bcb8f3241
wkdgusdnr55@gmail.com <wkdgusdnr55@gmail.com>
2022-08-31 18:41:55 +0900

    Add:댓글 추가 기능

--- src/components/Comment/EditComment.js
+++ src/components/Comment/EditComment.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import styled from "styled-components";
+import useContent from "hooks/useContent";
+import useAxios from "hooks/useAxios";
+import { api } from "redux/axios";
+import { toStringByFormatting } from "func/string";
+import { useParams } from "react-router-dom";
 
+const CommentInput = styled.input`
+    width: ${(props) => props.width || "512px"};
+`;
 const EditComment = () => {
-  
-
-  return <div></div>;
+    const axiosState = useAxios();
+    const params = useParams();
+    const [datas, onChange, reset] = useContent({
+        userName: "",
+        desc: "",
+    });
+    const addComment = async () => {
+        console.log(datas);
+        const result = await api.post("comment", {
+            ...datas,
+            isDeleted: false,
+            cardNum: params.id,
+            createdAt: toStringByFormatting(new Date()),
+        });
+        reset();
+        // window.location.reload();
+    };
+    return (
+        <span style={{ display: "flex", flexDirection: "column" }}>
+            <div>
+                <CommentInput
+                    width={"120px"}
+                    name="userName"
+                    placeholder="닉네임"
+                    onChange={onChange}
+                    value={datas.userName}
+                ></CommentInput>
+                <CommentInput
+                    name="desc"
+                    placeholder="댓글쓰기"
+                    onChange={onChange}
+                    value={datas.desc}
+                ></CommentInput>
+                <button onClick={addComment}>생성</button>
+            </div>
+        </span>
+    );
 };
 
 export default EditComment;
---
+++ src/func/string.js
@@ -0,0 +1,14 @@
+function leftPad(value) {
+    if (value >= 10) {
+        return value;
+    }
+
+    return `0${value}`;
+}
+export function toStringByFormatting(source, delimiter = "-") {
+    const year = source.getFullYear();
+    const month = leftPad(source.getMonth() + 1);
+    const day = leftPad(source.getDate());
+
+    return [year, month, day].join(delimiter);
+}
