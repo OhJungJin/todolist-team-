065be74d649885e5a48535f520e9429705b16be5
momoco-git <gnspope@naver.com>
2022-08-31 13:48:52 +0900

    커스텀훅 만드신거 추가

---
+++ src/hooks/useContent.js
@@ -0,0 +1,15 @@
+import { useState, useCallback } from "react";
+
+function useContent(initialForm) {
+  const [form, setForm] = useState(initialForm);
+  // change
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    setForm(prev => ({ ...prev, [name]: value }));
+  }, []);
+  // reset
+  const reset = useCallback(() => setForm(initialForm), [initialForm]);
+
+  return [form, onChange, reset, setForm];
+}
+export default useContent;
